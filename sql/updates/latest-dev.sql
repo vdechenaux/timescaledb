323d2d0bec5a97f9e0b60b9b9944b39a676c9556
gayyappan <gayathri@timescale.com>
2022-08-15 16:52:52 -0400

    latest-dev change

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -12,7 +12,12 @@ GRANT SELECT ON _timescaledb_catalog.dimension_partition TO PUBLIC;
 DROP FUNCTION IF EXISTS @extschema@.remove_continuous_aggregate_policy(REGCLASS, BOOL);
 
 -- add a new column to chunk catalog table 
-ALTER TABLE _timescaledb_catalog.chunk ADD COLUMN  osm_chunk boolean NOT NULL DEFAULT FALSE;
+ALTER TABLE _timescaledb_catalog.chunk ADD COLUMN  osm_chunk boolean ;
 UPDATE _timescaledb_catalog.chunk SET osm_chunk = FALSE;
+
+ALTER TABLE _timescaledb_catalog.chunk
+  ALTER COLUMN  osm_chunk SET NOT NULL;
+ALTER TABLE _timescaledb_catalog.chunk 
+  ALTER COLUMN  osm_chunk SET DEFAULT FALSE;
 
 CREATE INDEX chunk_osm_chunk_idx ON _timescaledb_catalog.chunk (hypertable_id, osm_chunk);
